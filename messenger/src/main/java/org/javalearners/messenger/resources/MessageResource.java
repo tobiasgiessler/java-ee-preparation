fc2c89f8adcdcfaaa5eb22e38a27d266cdf1e516
Tobias Gießler <developer@work.de>
2017-07-26 19:11:39 +0200

    Remove unused import.

--- messenger/src/main/java/org/javalearners/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/javalearners/messenger/resources/MessageResource.java
@@ -15,7 +15,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilderException;
 import javax.ws.rs.core.UriInfo;
 import org.javalearners.messenger.model.Message;
 import org.javalearners.messenger.resources.beans.MessageFilterBean;
