63fa00828e56cf0898e758a1a041869aecfd9891
Tobias Gießler <developer@work.de>
2017-08-26 14:21:19 +0200

    Make tests independent by removing all messages between tests.

--- messenger-test/src/test/java/org/javalearners/messenger/MessageResourceTest.java
+++ messenger-test/src/test/java/org/javalearners/messenger/MessageResourceTest.java
@@ -7,36 +7,44 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static io.restassured.RestAssured.*;
 import io.restassured.http.ContentType;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import java.lang.reflect.Array;
+import io.restassured.response.Response;
+import java.util.List;
 import javax.json.Json;
 import javax.json.JsonObject;
 import org.apache.http.HttpStatus;
 import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
 
-/**
- *
- * @author tobi
- */
 public class MessageResourceTest {
-    
+
+    private final String webapiPath = "http://localhost:8080/messenger/webapi";
+
     public MessageResourceTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
+    /**
+     * Deletes all present messages so that tests do not influence each other.
+     */
     @Before
     public void setUp() {
-        
+        final Response messages = get(webapiPath + "/messages");
+        messages.then().statusCode(HttpStatus.SC_OK);
+        final List<Integer> messageIds = messages.body().jsonPath().getList("id");
+
+        messageIds.stream().forEach(id -> {
+            delete(webapiPath + "/messages/" + id)
+                    .then()
+                    .statusCode(HttpStatus.SC_NO_CONTENT);
+        });
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -47,7 +55,7 @@ public class MessageResourceTest {
                 .then()
                 .statusCode(HttpStatus.SC_OK);
     }
-    
+
     @Test
     public void testAddMessage() {
         final JsonObject homerSaysHello = Json.createObjectBuilder()
@@ -57,7 +65,7 @@ public class MessageResourceTest {
         given()
                 .contentType(ContentType.JSON)
                 .body(homerSaysHello.toString())
-                .post("http://localhost:8080/messenger/webapi/messages")
+                .post(webapiPath + "/messages")
                 .then()
                 .statusCode(HttpStatus.SC_CREATED)
                 .body("author", equalTo("hsimpson"))
