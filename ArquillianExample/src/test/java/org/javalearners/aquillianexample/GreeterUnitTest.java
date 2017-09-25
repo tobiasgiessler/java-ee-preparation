9a6b869837db563461c2b8c79d795aa4e432a9c4
Tobias Gießler <developer@work.de>
2017-09-25 13:41:11 +0200

    Complete unit test example with alternative.

--- ArquillianExample/src/test/java/org/javalearners/aquillianexample/GreeterUnitTest.java
+++ ArquillianExample/src/test/java/org/javalearners/aquillianexample/GreeterUnitTest.java
@@ -1,10 +1,12 @@
 package org.javalearners.aquillianexample;
 
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import javax.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
@@ -12,15 +14,22 @@ import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
 public class GreeterUnitTest {
-    
+
     @Inject
     private Greeter greeter;
 
     @Deployment
     public static JavaArchive createDeployment() {
-        JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
+        final Path filePath = FileSystems.getDefault().getPath(
+                "src",
+                "test",
+                "resources",
+                "META-INF",
+                "beans.xml");
+        final File beansXml = filePath.toFile();
+        final JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
                 .addClasses(Greeter.class, PhraseBuilder.class, PhraseBuilderSimulator.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+                .addAsManifestResource(beansXml);
         System.out.println(jar.toString());
         return jar;
     }
@@ -29,9 +38,9 @@ public class GreeterUnitTest {
     public void testCreateGreeting() {
         System.out.println("createGreeting");
         String name = "Lisa";
-        String expResult = "test value!";
+        String expResult = "test value";
         String result = greeter.createGreeting(name);
         assertEquals(expResult, result);
     }
-    
+
 }
